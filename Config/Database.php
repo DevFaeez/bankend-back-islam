60f5b8fc3b3caafa5eabea3facfa6fdad31fb049
faiszuddin <muhdfaez.gm@gmail.com>
2025-06-21 23:42:05 +0800

    adjust the database

--- Config/Database.php
+++ Config/Database.php
@@ -1,25 +1,26 @@
-<?php 
+<?php
 namespace Config {
-class Database{
-    static function getConnection(): \PDO{
-        $port = 1521;
-        $host = "localhost:1521/FREEPDB1";
-        $username = "system";
-        $database = "bank_islam";
-        $password = "123456";
-        
-        try {
-            $connection = new \PDO("mysql:host=$host;port=$port;dbname=$database", $username, $password);
-            $connection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-            return $connection;
-        } catch (\PDOException $e) {
-            error_log("Database connection failed: " . $e->getMessage());
-            throw $e;
+    class Database
+    {
+        static function getConnection()
+        {
+            $host = "localhost:1521/FREEPDB1";
+            $username = "hr";
+            $password = "root";
+
+            $conn = oci_connect($username, $password, $host);
+
+            if (!$conn) {
+                $e = oci_error();
+                die("Connection failed: " . $e['message']);
+            }
+
+            return $conn;
         }
-        }
+
     }
 }
- 
+
 
 
 
